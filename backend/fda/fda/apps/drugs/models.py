6464a6ce83138b9c793c3c26b730e6b80644e00a
Cesar <cesarpino547@gmail.com>
2015-06-28 22:46:10 -0400

    [JIRA] GSA18F-33 Autocomplate Drug - get top 1000 drug names script, updated

--- backend/fda/fda/apps/drugs/models.py
+++ backend/fda/fda/apps/drugs/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Drug(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    field = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
