ee21973cd146d6c8bf97d6e429e40a2f3caf4620
andresgz <andresgz@gmail.com>
2015-06-29 23:43:45 -0400

    Gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -28,36 +28,27 @@ gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
 
-
 var awspublish = require('gulp-awspublish');
- 
-gulp.task('deploy', function() {
- 
-  // create a new publisher using S3 options 
-  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property 
-  // You need to define Environment variables: AWS_KEY, AWS_SECRET
+
+
+gulp.task('deploy', ['build'], function () {
+  // create a new publisher
   var publisher = awspublish.create({
-    params: {
-	    Key: process.env.AWS_KEY,
-	    Secret: process.env.AWS_SECRET,
-	    Bucket: 'gsa18f'
-    }
+  "params": {
+    "Bucket": process.env.AWS_BUCKET
+  },
+  "accessKeyId": process.env.AWS_KEY,
+  "secretAccessKey": process.env.AWS_SECRET
   });
- 
-  // define custom headers 
+
+  // define custom headers
   var headers = {
     'Cache-Control': 'max-age=315360000, no-transform, public'
-    // ... 
   };
 
-
   return gulp.src('dist/**/*.*')
-    // publisher will add Content-Length, Content-Type and headers specified above 
-    // If not specified it will set x-amz-acl to public-read by default 
     .pipe(publisher.publish(headers))
     .pipe(publisher.sync())
-    // create a cache file to speed up consecutive uploads 
     .pipe(publisher.cache())
-     // print upload updates to console 
     .pipe(awspublish.reporter());
 });
